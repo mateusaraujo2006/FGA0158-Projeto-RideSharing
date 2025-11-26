a5668751a921faf4a6a417ab07ba26368bf8bc1b
wandinhawright <wwanda.wwright@gmail.com>
2025-11-26 00:49:18 -0300

    adiciona veiculo e CNH

--- src/app/veiculo/Cnh.java
+++ src/app/veiculo/Cnh.java
@@ -1,4 +1,33 @@
 package app.veiculo;
 
 public class Cnh {
+    private final String Id;
+    private final String Validade;
+    private final String DatadeHoje;
+    
+    public Cnh(String id, String validade) {
+        this.Id = id;
+        this.Validade = validade;
+        this.DatadeHoje = DatadeHoje;
+        
+    }
+    public Boolean VerificarValidadeCnh () {
+        if (this.Id == null || this.Id.trim().isEmpty()) {
+            return false;
+        }
+        if (this.Validade == null || this.Validade.trim().isEmpty()) {
+            return false;
+        }
+        if (this.Id.length() < 9 || this.Id.length() > 9) {
+            return false;
+        }
+        if (validade < DatadeHoje){
+            return false;
+        }
+        else return true;
+
+    }
+
 }
+
+
