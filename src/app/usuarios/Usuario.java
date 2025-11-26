71a7fc72d3212c0ec3bbba9a338674882e95ad5b
unknown <osmarjunio7@gmail.com>
2025-11-26 14:22:36 -0300

    cadastro de avaliação e o comeco de usuario

--- src/app/usuarios/Usuario.java
+++ src/app/usuarios/Usuario.java
@@ -1,5 +1,20 @@
 package app.usuarios;
 
 public class Usuario {
+    private String nome,email,senha,cpf,telefone;
+    private Avaliacao avaliação;
 
+    public Usuario(String nome, String email, String senha, String cpf, String telefone, Avaliacao avaliação) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.avaliação=avaliação;
+
+    };
+    public void login(){
+        System.out.println("Insira o cpf:");
+
+    }
 }
